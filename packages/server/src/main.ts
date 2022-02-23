5f455adb086716212724c9bcaac8c1d5aea66884
CoolSnow <coolsnow2020@gmail.com>
2022-02-23 18:31:01 +0800

    refactor(server): listen port

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   //app.enableCors()
-  await app.listen(process.env.PORT || 3000)
+  await app.listen(3002)
   console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
