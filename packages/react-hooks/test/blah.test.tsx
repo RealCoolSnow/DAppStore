cd17e472909d0d1b5e58972748e2e38b361f4ac0
CoolSnow <coolsnow2020@gmail.com>
2022-03-01 18:18:41 +0800

    chore: update pnpm-lock.yaml

--- packages/react-hooks/test/blah.test.tsx
+++ packages/react-hooks/test/blah.test.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    const View = () => {
+      return <div>hello world</div>;
+    };
+    ReactDOM.render(<View />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
