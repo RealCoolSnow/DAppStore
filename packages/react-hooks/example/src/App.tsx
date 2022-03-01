94dcf82b7e7888bf6c96ddc2d5fa68fda4a7a2bd
CoolSnow <coolsnow2020@gmail.com>
2022-03-01 14:42:33 +0800

    feat: react-hooks\example

---
+++ packages/react-hooks/example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Img } from '../../src/use-image';
+
+function App() {
+  return (
+    <div className='App'>
+      <Img src={[]} />
+    </div>
+  );
+}
+
+export default App;
